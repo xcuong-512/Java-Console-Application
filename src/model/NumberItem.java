4376cfa6fe491afec5aebd42da4a99cd01aaae4e
TXcuon9 <xcuongai512@gmail.com>
2026-02-04 00:44:59 +0700

    create model & add menu

---
+++ src/model/NumberItem.java
@@ -0,0 +1,17 @@
+package model;
+
+public class NumberItem {
+    private  double value;
+
+    public NumberItem(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
